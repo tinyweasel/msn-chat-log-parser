f60a908f67c649f8af1100001acdd04c1fa3eeb2
Tanya Branagan <TanyaBranagan@gmail.com>
2020-08-24 22:23:56 +0100

    Format test with black

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,6 +3,8 @@ from parser import parse_chat_logs
 
 # TODO write better test
 def test_parse_chat_logs():
-  runner = CliRunner()
-  logs = runner.invoke(parse_chat_logs, ['--input=chat-logs', '--output=output-files'])
-  assert logs.exit_code == 1
+    runner = CliRunner()
+    logs = runner.invoke(
+        parse_chat_logs, ["--input=chat-logs", "--output=output-files"]
+    )
+    assert logs.exit_code == 1
