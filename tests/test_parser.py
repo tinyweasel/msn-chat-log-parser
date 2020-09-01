9c575c209849be0137717238c29fec029efe9f45
Tanya Branagan <TanyaBranagan@gmail.com>
2020-09-01 18:06:20 +0100

    Actually commit new folder this time

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -14,7 +14,7 @@ def test_parse_chat_logs():
     runner = CliRunner()
     logs = runner.invoke(
         parse_chat_logs,
-        ["--input-folder=tests/test-logs", "--output-folder=test-output-files", "--print-output"],
+        ["--input-folder=tests/test-logs", "--output-folder=tests/test-output-files", "--print-output"],
     )
     assert logs.exit_code == 0
     assert logs.output == "22:46:40 06/07/2007\nJohn - Hello\n\n22:46:41 06/07/2007\nJeff - I am part of a test\n\nFinished parsing.\n"
