6b8f1ee21192630cc52b7a5651e62dd0983da704
Tanya Branagan <TanyaBranagan@gmail.com>
2020-09-03 16:51:15 +0100

    Add type hints and a test

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # MSN Chat log parser
 
 This parser uses Beautiful Soup to convert MSN Messenger chat logs from XML to a more readable text file.
+If the input and output folder and print boolean aren't set, it will prompt the user to enter them.
 
 ## Usage
 
+### To print chat logs to console
 `pipenv run python parser.py --input-folder=<input_folder> --output-folder=<output_folder> --print-output`
+
+
+### To output formatted text files without printing to console
+
+`pipenv run python parser.py --input-folder=<input_folder> --output-folder=<output_folder> --no-print`
 
 ### To run tests
 
@@ -12,6 +19,4 @@ This parser uses Beautiful Soup to convert MSN Messenger chat logs from XML to a
 
 #### TODO
 
-* Add proper tests
-* Add better docstrings
-* Add typings
+* Create functions to read/write to file
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,16 +1,10 @@
+from bs4 import BeautifulSoup
 from click.testing import CliRunner
+
 from parser import format_message, parse_chat_logs
 
-# TODO mock Beautiful soup
-def test_format_message():
-    # message_to_format = "test"
-    # formatted_message = format_message(message_to_format)
 
-    # assert formatted_message == "test"
-    pass
-
-
-def test_parse_chat_logs():
+def test_parse_chat_logs_prints_chat_logs_to_console():
     runner = CliRunner()
     logs = runner.invoke(
         parse_chat_logs,
@@ -18,3 +12,12 @@ def test_parse_chat_logs():
     )
     assert logs.exit_code == 0
     assert logs.output == "22:46:40 06/07/2007\nJohn - Hello\n\n22:46:41 06/07/2007\nJeff - I am part of a test\n\nFinished parsing.\n"
+
+def test_parse_chat_logs_does_not_print_logs():
+    runner = CliRunner()
+    logs = runner.invoke(
+        parse_chat_logs,
+        ["--input-folder=tests/test-logs", "--output-folder=tests/test-output-files", "--no-print"],
+    )
+    assert logs.exit_code == 0
+    assert logs.output == "Finished parsing.\n"
